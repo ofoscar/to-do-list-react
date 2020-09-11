fc61cb972e78044086be8149662bdbe837ee2d4a
Oscar R <ktaoscar@gmail.com>
2020-09-11 14:16:39 -0600

    complete handler

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ function App() {
       setTodos={setTodos} 
       setInputText={setInputText}        
       />
-      <TodoList/>
+      <TodoList 
+      setTodos={setTodos} 
+      todos={todos}
+      />
     </div>
   );
 }
