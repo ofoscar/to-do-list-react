e9d048f8ed98d5b5d455b27b28b01f842c6acaf7
Oscar R <ktaoscar@gmail.com>
2020-09-11 13:45:41 -0600

    agregando componente de lista

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form'
+import TodoList from './components/Todolist'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
       <h1>OFOscar Todo list</h1>
       </header>
-      <Form/>
+      <Form 
+      inputText={inputText}
+      todos={todos}
+      setTodos={setTodos} 
+      setInputText={setInputText}        
+      />
+      <TodoList/>
     </div>
   );
 }
